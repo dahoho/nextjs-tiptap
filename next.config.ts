cba2bae7f379a94a54c95b99a3f9175c3de7d575
Ryosuke Hoda <h.ryosuke820@gmail.com>
2025-02-08 19:42:26 +0900

    webpack設定を更新し、クライアントサイドでの'yjs'インポートのインスタンスを統一

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 他の設定オプションをここに記述
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // 全ての 'yjs' のインポートが同一のインスタンスを参照するようにする
+      config.resolve.alias["yjs"] = path.resolve(__dirname, "node_modules/yjs");
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
