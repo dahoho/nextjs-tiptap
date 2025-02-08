14aaa6a1b250022c9b0cd25d7769f73370ea7566
Ryosuke Hoda <h.ryosuke820@gmail.com>
2025-02-08 21:49:55 +0900

    Tiptapエディタのスタイルを更新し、見出しの設定を追加

--- src/components/Tiptap/_containers/container.tsx
+++ src/components/Tiptap/_containers/container.tsx
@@ -6,8 +6,16 @@ import { TiptapPresentational } from "./presentational";
 
 export const TiptapContainer = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [StarterKit.configure({ heading: { levels: [2, 3] } })],
     content: "<p>Hello World! 🌎️</p>",
+    editorProps: {
+      attributes: {
+        class: "bg-gray-100 p-4",
+      },
+    },
+    onUpdate({ editor }) {
+      console.log(editor.getHTML());
+    },
   });
   return <TiptapPresentational editor={editor} />;
 };
